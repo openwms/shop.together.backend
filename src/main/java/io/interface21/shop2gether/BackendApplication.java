676093e4820a3d29cdd44e5286c107b33e05e986
Heiko Scherrer <scherrer@openwms.org>
2016-11-25 19:52:40 +0100

    Initial version

---
+++ src/main/java/io/interface21/shop2gether/BackendApplication.java
@@ -0,0 +1,12 @@
+package io.interface21.shop2gether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+}
---
+++ src/test/java/io/interface21/shop2gether/BackendApplicationTests.java
@@ -0,0 +1,16 @@
+package io.interface21.shop2gether;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
