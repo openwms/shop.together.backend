195381df07f3682d2e5492f8969949f808d97225
Heiko Scherrer <scherrer@openwms.org>
2017-02-09 20:04:05 +0100

    #14 Jackson transforms an array with one element into an object, restrict this, always go with arrays

--- src/main/java/io/interface21/shop2gether/BackendApplication.java
+++ src/main/java/io/interface21/shop2gether/BackendApplication.java
@@ -1,5 +1,8 @@
 package io.interface21.shop2gether;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.ameba.annotation.EnableAspects;
 import org.ameba.mapping.BeanMapper;
 import org.ameba.mapping.DozerMapperImpl;
@@ -7,6 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -27,5 +31,19 @@ public class BackendApplication {
     @Bean
     BeanMapper mapper() {
         return new DozerMapperImpl("META-INF/dozer/bean-mappings.xml");
+    }
+
+    public
+    @Primary
+    @Bean
+    com.fasterxml.jackson.databind.ObjectMapper jackson2ObjectMapper() {
+        com.fasterxml.jackson.databind.ObjectMapper om = new com.fasterxml.jackson.databind.ObjectMapper();
+        om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        om.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+        om.configure(SerializationFeature.WRITE_EMPTY_JSON_ARRAYS, true);
+        om.configure(SerializationFeature.INDENT_OUTPUT, true);
+        om.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, true);
+        om.setSerializationInclusion(JsonInclude.Include.ALWAYS);
+        return om;
     }
 }
