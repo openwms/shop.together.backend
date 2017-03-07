c2bfd40cecd1e359b8b0b62ed842126edc4d5747
Heiko Scherrer <scherrer@openwms.org>
2017-03-07 09:10:14 +0100

    #25 implemented at least the link correctly. Implementation of UserGroup controller will happen when needed

--- src/main/java/io/interface21/shop2gether/ItemController.java
+++ src/main/java/io/interface21/shop2gether/ItemController.java
@@ -1,7 +1,11 @@
 package io.interface21.shop2gether;
 
-import org.springframework.hateoas.Link;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -28,9 +32,9 @@ class ItemController {
 
             // enrich
             item.getSharedWith().forEach(i -> {
-                item.add(linkTo(methodOn(UserGroupController.class).getItemFor(i.get()))
-                        .withRel("_items"));
-                item.add(new Link("https://shop2gether.herokuapp.com/usergroups/" + i.getPk(), "usergroups"));
+                item.add(linkTo(methodOn(UserGroupController.class).getUserGroupFor
+                        (item.getPersistentKey())).withRel("usergroups"));
+            });
         }
         return item;
     }
